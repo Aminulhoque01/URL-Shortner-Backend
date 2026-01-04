aa5339b95c8935d21f9a14ad4f65bf776cebd7d4
Aminulhoque01 <aminuhoque53@gmail.com>
2026-01-04 22:23:17 +0600

    first commit

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+dotenv.config();
+
+ 
+ 
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+ 
+
+ 
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/url", urlRoutes);
+
+ 
+
+
+const PORT = process.env.PORT || 5000;
+
+(async () => {
+  await connectDB();
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+})();
