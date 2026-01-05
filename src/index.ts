4cbf4647f180fcefad04a6e66d487027b0edb3ea
Aminulhoque01 <aminuhoque53@gmail.com>
2026-01-05 11:24:59 +0600

    server is runing

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,23 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { connectDB } from "./config/db.js";
-dotenv.config();
+import { connectDB } from "./config/db";
 
- 
- 
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
- 
 
- 
-
-// app.use("/api/auth", authRoutes);
-// app.use("/api/url", urlRoutes);
-
- 
-
-
+app.get("/", (req, res) => {
+  res.send({ message: "server is running" });
+});
 const PORT = process.env.PORT || 5000;
 
 (async () => {
   await connectDB();
-
   app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`🚀 Server running on port ${PORT}`);
   });
 })();
